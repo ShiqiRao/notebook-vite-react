6b9e9352261cb78269a96e2cc13e023ed55adfe5
d3z1on <nothinover@sina.com>
2021-06-23 20:29:46 +0800

    侧边栏

--- src/components/LeftNav/LeftNav.tsx
+++ src/components/LeftNav/LeftNav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './LeftNav.scss';
 
 function LeftNav() {
@@ -7,46 +7,48 @@ function LeftNav() {
         <div className='left-nav__avatar'>
             <img src='https://avatars.githubusercontent.com/u/12002442?s=200&v=4'></img>
         </div>
-        <Link to="/note">
-            <div className='left-nav__item'>
-                <div className='left-nav__icon'>
-                    <img src='https://avatars.githubusercontent.com/u/12002442?s=200&v=4'></img>
-                </div>
-                <div>
-                    笔记
-                </div>
+        <NavLink className='left-nav__item' to="/note">
+            <div className='left-nav__icon left-nav__icon__note'>
             </div>
-        </Link>
-        <Link to="/photo">
-            <div className='left-nav__item'>
-                <div className='left-nav__icon'>
-                    <img src='https://avatars.githubusercontent.com/u/12002442?s=200&v=4'></img>
-                </div>
-                <div>
-                    图片
-                </div>
+            <div>
+                笔记
             </div>
-        </Link>
-        <Link to="/audio">
-            <div className='left-nav__item'>
-                <div className='left-nav__icon'>
-                    <img src='https://avatars.githubusercontent.com/u/12002442?s=200&v=4'></img>
-                </div>
-                <div>
-                    音频
-                </div>
+        </NavLink>
+        <NavLink className='left-nav__item' to="/photo">
+            <div className='left-nav__icon left-nav__icon__pic'>
             </div>
-        </Link>
-        <Link to="/star">
-            <div className='left-nav__item'>
-                <div className='left-nav__icon'>
-                    <img src='https://avatars.githubusercontent.com/u/12002442?s=200&v=4'></img>
-                </div>
-                <div>
-                    星标
-                </div>
+            <div>
+                图片
             </div>
-        </Link>
+        </NavLink>
+        <NavLink className='left-nav__item' to="/audio">
+            <div className='left-nav__icon left-nav__icon__audio'>
+            </div>
+            <div>
+                音频
+            </div>
+        </NavLink>
+        <NavLink className='left-nav__item' to="/folder">
+            <div className='left-nav__icon left-nav__icon__folder'>
+            </div>
+            <div>
+                分类
+            </div>
+        </NavLink>
+        <NavLink className='left-nav__item' to="/star">
+            <div className='left-nav__icon left-nav__icon__collection'>
+            </div>
+            <div>
+                星标
+            </div>
+        </NavLink>
+        <NavLink className='left-nav__item' to="/ashbin">
+            <div className='left-nav__icon left-nav__icon__ashbin'>
+            </div>
+            <div>
+                回收
+            </div>
+        </NavLink>
     </div>
 }
 
