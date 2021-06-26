ced90bc0091b152d2b7808554494334e8b511504
d3z1on <nothinover@sina.com>
2021-06-26 17:30:43 +0800

    🐞 fix(redux): 修复redux相关报错

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -4,14 +4,14 @@ import clockIcon from "../../assets/images/clock.svg";
 import searchIcon from "../../assets/images/search.svg";
 import { INote } from "../../common/INote";
 import Model from "../../common/Model";
-import { update } from '../../reducer/note';
+import { selectNote, update } from '../../reducer/note';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import "./NoteList.scss";
 
 function NoteList() {
     const [model, setModel] = useState<Model>(new Model())
     const [list, setList] = useState<INote[]>([])
-    const note = useAppSelector(state => state.note.currentNote)
+    const note = useAppSelector(selectNote)
     const dispatch = useAppDispatch()
     useEffect(() => {
         initData()
@@ -32,19 +32,15 @@ function NoteList() {
             })
     }
 
-    function handleSelectNote(note: INote) {
-        update({
-            payload: note
-        })
-    }
-
     return <div className="note-list">
         <div className="note-list__topbar">
             <input></input>
             <img className="note-list__search" src={searchIcon}></img>
             <div onClick={handleAddNote} className="note-list__add"></div>
         </div>
-        {list.map(item => <div key={item.id} onClick={() => handleSelectNote(item)} className="note-list__note">
+        {list.map(item => <div key={item.id} onClick={() => dispatch(update({
+            payload: item
+        }))} className="note-list__note">
             <div className="note-list__title">{format(new Date(item.create_at), "yyyy-MM-dd")}</div>
             <div className="note-list__detail">{item.content}</div>
             <div className="note-list__time">
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import noteReducer from '../reducer/note'
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
       note: noteReducer
   },
