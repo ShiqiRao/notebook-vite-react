e4101fd306eb5e294260062b120109a8b7ed1ecb
d3z1on <nothinover@sina.com>
2021-07-14 21:44:45 +0800

    ✨ feat(folder): 新增folder页面

---
+++ src/pages/Folder/Folder.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Search from "../../components/Search/Search"
+
+function Folder() {
+    return <div>
+        <Search></Search>
+    </div>
+}
+
+export default Folder
